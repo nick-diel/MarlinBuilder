21dcb55f63b3b87669684fcb4e9553f501586afe
Nick Diel <nick@nickdiel.com>
2020-03-12 11:17:37 -0600

    fixed script permission

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,6 @@ RUN curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/inst
 
 # Copy Build File
 COPY ./compile.sh /usr/local/bin
+RUN chmod +x /usr/local/bin/compile.sh
 
 ENTRYPOINT [ "compile.sh" ]
--- compile.sh
+++ compile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #check to make sure there is an argument
 
-#find and set objcopy
-OBJCOPY=$(find $HOME/.arduino15/packages/STM32/tools/arm-none-eabi-gcc -name "objcopy")
-
 #update index and install STM32 package, specify version as command line
 arduino-cli core update-index --additional-urls https://github.com/stm32duino/BoardManagerFiles/raw/master/STM32/package_stm_index.json
 arduino-cli core install STM32:stm32@$1 --additional-urls https://github.com/stm32duino/BoardManagerFiles/raw/master/STM32/package_stm_index.json
+
+#find and set objcopy
+OBJCOPY=$(find $HOME/.arduino15/packages/STM32/tools/arm-none-eabi-gcc -name "objcopy")
 
 #copy src for speed improvement
 cp -r /usr/src/marlin/ /tmp/src/
